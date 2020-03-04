00c861362b3cdcceb71b402fc94b4b8c35f49a39
Janne Mattila <janne.mattila@hotmail.fi>
2020-03-04 21:58:12 +0200

    Update motd

--- init_container.sh
+++ init_container.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
-
 # Based on https://github.com/Azure-App-Service/dotnetcore/blob/master/2.2/Dockerfile
+
+cat >/etc/motd <<EOL 
+     _
+    | | __ _ _ __  _ __   ___
+ _  | |/ _` | '_ \| '_ \ / _ \
+| |_| | (_| | | | | | | |  __/
+ \___/ \__,_|_| |_|_| |_|\___|
+ App Service remote access using SSH
+
+GitHub: https://github.com/JanneMattila/327-webapp-remote-access
+Docker Hub: https://hub.docker.com/r/jannemattila/webapp-remote-access
+
+.NETCore runtime version: `ls -X /usr/share/dotnet/shared/Microsoft.NETCore.App | tail -n 1`
+EOL
+cat /etc/motd
 
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
