6043a41e6b1e1ed8db6c502b31dff2f237835aa6
Janne Mattila <janne.mattila@hotmail.fi>
2020-03-04 19:52:38 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Web app and remote access
+
+## Build Status
+
+[![Build Status](https://dev.azure.com/jannemattila/jannemattila/_apis/build/status/JanneMattila.326-webapp-and-folders?branchName=master)](https://dev.azure.com/jannemattila/jannemattila/_build/latest?definitionId=45&branchName=master)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ src/WebApp/Controllers/FilesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly ILogger<FilesController> _logger;
+
+        public FilesController(ILogger<FilesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var content = "No content";
+            const string exampleFile = "example.txt";
+
+            if (System.IO.File.Exists(exampleFile))
+            {
+                content = await System.IO.File.ReadAllTextAsync(exampleFile);
+            }
+            return content;
+        }
+    }
+}
